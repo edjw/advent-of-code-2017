ce703d82f551e3d7004cd0f9f7d7868cc27b6c0a
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2017-12-04 10:36:11 +0000

    switching itertools.permutations to itertools.combinations as it's faster here

--- 04_two.py
+++ 04_two.py
@@ -1,4 +1,4 @@
-from itertools import permutations
+from itertools import combinations
 
 new_file = []
 number_of_passphrases = 0 #lines
@@ -18,9 +18,10 @@ for line in new_file:
         word = ''.join(sorted(word)) # sorts each word in the line alphabetically
         sorted_line_list.append(word) # adds the sorted words into a fresh list
 
-    all_permutations_sorted_line_list = list(permutations(sorted_line_list, 2)) # makes a list of all permutations of the sorted list
+    all_combinations_sorted_line_list = list(combinations(sorted_line_list, 2)) # makes a list of all combinations of the sorted list
+    # slightly faster than permutations because will match quicker below
 
-    for pairing in all_permutations_sorted_line_list:
+    for pairing in all_combinations_sorted_line_list:
         if pairing[0] == pairing[1]: # if the words match then there's an anagram
             number_of_passphrases_containing_anagrams += 1 # count that there's a matched word
             break # break so it only counts the first match of each line
