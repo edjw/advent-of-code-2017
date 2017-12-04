1c9f4a557b53c7d6eb32e89abd619dc4e8380f2e
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2017-12-04 10:43:02 +0000

    neatening up d4p1

--- 04_one.py
+++ 04_one.py
@@ -1,15 +1,8 @@
-new_file = []
 counter = 0
 
 with open('input/4.txt', 'r') as f:
     for line in f:
-        line = line.split() # makes a list out of the current string line, removes tabs and \n
-        line = list(line) # makes the line a list
-        new_file.append(line) # appends the line list to a fresh list ## not sure why I need this tbh
-
-for line in new_file:
-    if len(line) == len(set(line)):
-    # if items in line is same as number of items in line with duplicates removed
-        counter += 1
-        # then add 1 to counter
+        line = line.strip().split() # strip removes tabs and \n. split makes a list out of the current string line
+        if len(line) == len(set(line)):
+            counter += 1
 print(counter)
